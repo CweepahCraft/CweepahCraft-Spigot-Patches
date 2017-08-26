b748bf40a4030b82eb823a0be5924b5eebf56a36
Jacob Martin <jjm_223@hotmail.com>
2017-08-26 10:19:13 -0500

    Omit stats and patch numbers from patch files

--- regenPatches.sh
+++ regenPatches.sh
@@ -5,11 +5,11 @@ set -e
 cd ./Spigot-API-Patches/
 rm *
 cd ../CweepahCraft-API/
-git format-patch -o ../Spigot-API-Patches/ origin/master
+git format-patch --no-stat -N -o ../Spigot-API-Patches/ origin/master
 
 cd ../Spigot-Server-Patches/
 rm *
 cd ../CweepahCraft-Server/
-git format-patch -o ../Spigot-Server-Patches/ upstream/master
+git format-patch --no-stat -N -o ../Spigot-Server-Patches/ upstream/master
 
 echo Done!
